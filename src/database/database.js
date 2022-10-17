2217ea050d2485e658df0f91f73bf1735bb1e433
kcandeloni <kcandeloni@gmail.com>
2022-10-17 00:24:53 -0300

    Preparação para o Deploy

--- src/database/database.js
+++ src/database/database.js
@@ -5,11 +5,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const connection = new Pool({
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default connection;
